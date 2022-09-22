f935513e498c5252ac4138cb921615d0942c2f2a
DGathu <kinyuagathu@gmail.com>
2022-09-22 09:46:59 +0000

    relations adjustments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     print("{:d} {:s}{:s}".
           format(size,
                  "arguments" if (size) != 1 else "argument",
-                 "." if (size) is 0 else ":"))
+                 "." if (size) == 0 else ":"))
     for idx, arg in enumerate(userin):
         print("{:d}: {:s}".format(idx + 1, arg))
